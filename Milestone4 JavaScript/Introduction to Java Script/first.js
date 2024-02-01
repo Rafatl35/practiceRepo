668302fa2214b062445865154bc0104c56855991
Rafatl35 <rafatrahmano4@gmail.com>
2024-02-01 08:21:52 +0600

    variable,operation

---
+++ Milestone4 JavaScript/Introduction to Java Script/first.js
@@ -0,0 +1,28 @@
+// 5 things to declare a variable
+var weight = 38;
+var price = 38;
+var year = 1995;
+var money = 2005;
+
+var age; /* undefined */
+console.log(money);
+console.log(price);
+
+// 1. no keyword in variable name
+ var false = 45;
+ var var = 54;
+
+// 2. no space or gap in variable name
+var is happy = false; // error
+
+// 3. No quote
+ var "address" = "abc"; // error
+
+// 4. can not start a number
+ var 1abc = 54 // error 
+ var abc1 =55 // no error
+
+// 5. name is case sensitive
+
+
+
---
+++ Milestone4 JavaScript/Introduction to Java Script/number.js
@@ -0,0 +1,28 @@
+// var orangePrice = 20;
+// var chocolate = 0.5;
+// var applePrice = '26'; //26 is string
+
+// console.log(typeof orangePrice);
+// console.log(typeof chocolate);
+// console.log(typeof applePrice);
+
+// Operations
+var eggPrice = 10;
+var onionPrice = 20;
+
+console.log(eggPrice + onionPrice);
+console.log(onionPrice - eggPrice);
+console.log(onionPrice * eggPrice);
+console.log(onionPrice / eggPrice);
+
+// remainder
+console.log(10 % 3);
+console.log(11 % 7);
+
+var price = 35;
+price = price + 10;
+console.log(price);
+
+var newPrice = price + 50;
+newPrice += 10; //newPrice = newPrice + 10
+console.log(newPrice);
