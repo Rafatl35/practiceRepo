08a7a8129209442309c58b639a69e75bf776ba33
Rafatl35 <rafatrahmano4@gmail.com>
2024-02-03 17:51:44 +0600

    for loop

---
+++ Milestone4 JavaScript/for-loop/odd & even.js
@@ -0,0 +1,22 @@
+// even
+// for(let i = 0; i <= 20; i=i+2){
+//     console.log(i);
+// }
+
+// odd
+// for(let i = 1; i <= 20; i=i+2){
+//     console.log(i);
+// }
+ 
+// sum
+// let sum = 0;
+// for(let i = 1; i <= 10; i++){
+//     sum = sum + i;
+//     console.log(i);
+// }
+// console.log(sum);
+
+// decremental for loop
+for(let i = 10; i >= 0 ; i--){
+    console.log(i);
+}
