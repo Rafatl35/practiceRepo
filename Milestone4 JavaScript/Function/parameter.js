3f41dbc4de66bde861666e3f67809f07df60d559
Rafatl35 <rafatrahmano4@gmail.com>
2024-02-08 23:28:56 +0600

    .

--- Milestone4 JavaScript/Function/parameter.js
+++ Milestone4 JavaScript/Function/parameter.js
@@ -9,3 +9,21 @@ function square(number){
     console.log('squre of the given number is: ', square1);
 }
 square(4);
+square(5);
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    console.log('total sum:', sum);
+
+}
+add(4, 54);
+
+function addAll(a, b, c, d, e){
+    const total = a + b + c + d + e;
+    console.log(a, b, c, d, e);
+    console.log('total sum :',total)
+
+}
+addAll(4, 5, 6, 6, 2);
+addAll(1, 3, 5, 6); //undefined 
+
