32ad465034ad8e27923dd889fce8b01908f0a20d
janossofsys <gitrepository@janos.com.bd>
2024-02-08 17:55:14 +0600

    added the function

---
+++ Milestone4 JavaScript/Function/function.js
@@ -0,0 +1,19 @@
+// function name(){
+
+// }
+
+
+const price = 55;
+// function declare
+function box1(){
+    console.log('fan');
+    console.log('ball');
+    console.log('pen')
+}
+function box2(){
+    console.log('book');
+    console.log('phone');
+    console.log('laptop')
+}
+// call the function
+box1();
---
+++ Milestone4 JavaScript/Function/parameter.js
@@ -0,0 +1,11 @@
+// function square (number){
+//     console.log(number);
+// }
+// square(4);
+
+function square(number){
+    console.log('value of the number parameter', number);
+    const square1 = number * number;
+    console.log('squre of the given number is: ', square1);
+}
+square(4);
