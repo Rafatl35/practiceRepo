b1eaa0d62d31fea6d92de1b43e617a84734fd80e
janossofsys <gitrepository@janos.com.bd>
2024-02-10 16:38:33 +0600

    .

--- Milestone4 JavaScript/Function/even-only.js
+++ Milestone4 JavaScript/Function/even-only.js
@@ -4,11 +4,31 @@
  */
 
 function evenNumbersOnly(numbers){
+    const events = [];
     for(const number of numbers){
         if(number % 2 === 0){
             console.log(number);
+            events.push(number)
         }
     }
+    return events;
 }
 const numbers = [2, 5, 6, 3, 10, 8]
-evenNumbersOnly(numbers);
+const events = evenNumbersOnly(numbers);
+console.log('event numbers are :', events);
+
+console.log('Sum of Even Numbers------------------------------');
+
+function sumOfEvenNumbers(numbers){
+    let sum = 0;
+    for(const number of numbers){
+        if(number % 2 === 0){
+            console.log(number);
+            sum = sum + number;
+        }
+    }
+    return sum;
+}
+
+const sum = sumOfEvenNumbers(numbers);
+console.log('sum of the even numbers is :', sum);
