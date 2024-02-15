58c55f3d517b917990595fec84e9ebfacfcaa7eb
Rafatl35 <rafatrahmano4@gmail.com>
2024-02-15 23:25:36 +0600

    .

---
+++ Milestone4 JavaScript/Simple Coding Problems/discount.js
@@ -0,0 +1,59 @@
+/**
+ * upto 100: ---> 100
+ * more than 101-200: --> 90
+ * more than 200: --->70
+ */
+
+function discountedPrice(quantity){
+    if(quantity <= 100){
+        const total = quantity * 100;
+        return total;
+    }
+    else if(quantity <= 200){
+        const total = quantity * 90;
+        return total;
+    }
+    else{
+        const total = quantity * 70;
+        return total;
+    }
+
+}
+const total = discountedPrice(5);
+console.log(total);
+
+/**
+ * lear discount 
+ * first100 ---> 100
+ * 101-200 ---> 90
+ * above 200 ---> 70
+ */
+
+function layeredDiscountedTotal(quantity){
+    const first100Price = 100;
+    const second100Price = 90;
+    const above200Price = 70;
+
+    if(quantity <= 100){
+        const total = quantity * first100Price;
+        return total;
+    }
+    else if(quantity <= 200){
+        const first100Total = 100 * first100Price;
+        const remainingQuantity = quantity - 100;
+        const remainingTotal = remainingQuantity * second100Price;
+        const total = first100Total + remainingTotal;
+        return total;
+    }
+    else{
+        const first100Total = 100 * first100Price;
+        const second100Total = 100 * second100Price;
+        const remainingQuantity = quantity - 200;
+        const remainingTotal = remainingQuantity + above200Price;
+        const total = first100Total + second100Total + remainingTotal;
+        return total;
+    }
+}
+
+const totalPrice = layeredDiscountedTotal(200);
+console.log(totalPrice);
