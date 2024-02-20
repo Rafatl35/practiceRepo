c2584a4be670338285b65e8b74bf9a35855304a1
Rafatl35 <rafatrahmano4@gmail.com>
2024-02-20 17:38:48 +0600

    .

--- Milestone4 JavaScript/Assignment/assignment.js
+++ Milestone4 JavaScript/Assignment/assignment.js
@@ -1,1 +1,14 @@
-   
+function filterNumbers(arr) {
+    if (!Array.isArray(arr)) {
+      return "Error: Input is not an array.";
+    }
+  
+    const filteredArray = arr.filter(element => typeof element === 'number' && !isNaN(element));
+  
+    return filteredArray;
+  }
+  
+  // Example usage:
+  const inputArray = {num: [ 1 , 2 , 3 ]}   ;
+  const outputArray = filterNumbers(inputArray);
+  console.log(outputArray); // Output: [1, 18, -19]
