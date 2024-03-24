24812fbc1d4c57388e83f78431cef70ebbcd701c
Rafatl35 <gitrepository@janos.com.bd>
2024-03-24 15:39:50 +0600

    dom

---
+++ Milestone4 JavaScript/Milestone5 JavaScript dom/Play with dom/script.js
@@ -0,0 +1,41 @@
+console.log("connection");
+
+// const title = document.getElementById('title');
+// title.innerText = 'My Js power';
+// title.style.color = "red"
+// console.log(title.innerText);
+
+// const title2 = document.querySelector("p");
+// title2.textContent = "I am done by selector";
+// console.log(title2);
+
+// const items = document.getElementsByClassName("li-items");
+// console.log(items.length);
+
+// const myArr = Array.from(items);
+// console.log(myArr);
+
+// for(let index = 0; index < items.length; index++){
+//     const element = items[index];
+//     element.style.color = "green";
+//     // console.log(element);
+// }
+
+// const items2 = document.getElementsByTagName("li");
+// console.log(items2);
+
+// for(const iterator of items2){
+//     console.log(iterator);
+// }
+
+// const items3 = document.querySelectorAll("li");
+// console.log(items3);
+
+// const bodyName = document.querySelector("body");
+// console.log(bodyName.children);
+
+// const ul = document.getElementById("li-container");
+// console.log(ul.previousElementSibling.nextElementSibling);
+
+const ul = document.querySelector('ul');
+ul.innerHTML = "<p>hello</p>"
